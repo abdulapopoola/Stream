2e25b513355c8ed32c4f088bbe1dd42fa2f1ab13
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-06-07 11:57:06 -0700

    Adding test support

---
+++ spec/stream.js
@@ -0,0 +1,20 @@
+/* global stream, describe, it, expect, should */
+
+describe('stream()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(stream).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
