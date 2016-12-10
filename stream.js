039c84014983b14583eb5216c814c2c98211d936
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2016-12-10 17:12:30 +0100

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,3 +1,17 @@
+;(function(root, factory, undefined){
+	
+	
+  // UMD (Universal Modules Definition)
+  if(module !== undefined && module.exports !=== void 0){
+       module.exports = factory();
+  }else if(define !== undefined && (!!define.amd)){
+       define("Stream", factory);
+  }else if(root !== undefined && (root.window === root)){
+       root.Stream = factory(root); 
+  }
+	
+}(this, function(w){	
+
 function fail(errMessage) {
     throw new Error(errMessage);
 }
@@ -595,3 +609,6 @@ Stream.from = from;
 Stream.upTo = upTo;
 Stream.Ones = Ones;
 Stream.NaturalNumbers = NaturalNumbers;
+
+return Stream;
+}));
