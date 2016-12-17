ef1aaadd9bce0bcb0eff4019ff36ce5d7dde9719
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-12-17 14:33:31 -0800

    added code coverage support

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,27 @@
+var istanbul = require('browserify-istanbul');
+
 module.exports = function (config) {
     config.set({
+        browserify: {
+            debug: true,
+            transform: [
+                istanbul({
+                    ignore: ['**/node_modules/**']
+                })
+            ],
+            verbose: true
+        },
+        browsers: ['PhantomJS'],
+        coverageReporter: {
+            // type: 'html',
+            dir: 'tests/coverage/',
+            reporters: [{
+                type: 'text-summary'
+            }, {
+                type: 'lcov',
+                subdir: './'
+            }]
+        },
         frameworks: ['browserify', 'jasmine'],
         files: [
             'src/**/*.js',
@@ -9,19 +31,25 @@ module.exports = function (config) {
             'tests/**/*.js': ['jshint', 'browserify'],
             'src/**/*.js': ['jshint', 'browserify']
         },
-        browsers: ['PhantomJS'],
-        browserify: {
-            debug: true,
-            verbose: true
-        },
         port: 9870,
-        reporters: ['spec', 'html'],
+        reporters: [
+            'spec',
+            'html',
+            'coverage',
+            'threshold'
+        ],
         htmlReporter: {
             outputFile: 'tests/tests.html',
             pageTitle: 'Stream Unit Tests',
             subPageTitle: 'Stream tests',
             groupSuites: true,
             useCompactStyle: true
+        },
+        thresholdReporter: {
+            statements: 90,
+            branches: 90,
+            functions: 90,
+            lines: 90
         }
     });
 };
